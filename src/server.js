5e470a940342b8d93bbfb62e2ae8775adb68b971
katya-petrova <walki-disi@ya.ru>
2021-11-30 23:29:41 +0300

    refactor: replace express with fastify

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+app.listen(PORT, (err, address) => {
+    if (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
+    app.log.info(`server listening on ${address}`)
+  })
